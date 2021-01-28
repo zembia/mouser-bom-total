64e79e69930637f390361daeb19f02fff21f7e92
Nicolás Celedón <nicolas.celedon@zembia.cl>
2021-01-28 11:42:12 -0300

    feat: Updates the total when changing visible rows

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This chrome extension does just that. When you see a BOM, it will sum and show total of all the products added in the BOM.
 
+**NOTE**: It takes into account ONLY THE ITEMS SHOWN ON THE PAGE.
+
 # License
 
 This extension is under MIT license. See `LICENSE` for more info.
--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 const updateWebsite = () => {
+  console.log('test');
   // Retrieve rows with the price
   const prices = document.querySelectorAll(
     '.row.pricebreaks-row.active .col-xs-4.bold'
@@ -17,13 +18,32 @@ const updateWebsite = () => {
     currency: 'USD',
   });
 
-  // Insert a new row on top of the table
-  const row = document.querySelector('.cart-body').insertRow(0);
+  const divNotCreated = document.querySelector('.mouser-bom-total') === null;
 
-  // Adds the Total cost to the HTML
-  row.innerHTML = `<td colspan="10" style="text-align: center; background-color: #dff0d8;"><span style="font-weight: bold; font-size: 16px; margin-top: 8px; margin-bottom: 8px;">TOTAL ${formatter.format(
-    totalPrice
-  )}</span></td>`;
+  if (divNotCreated) {
+    // Adds the Total cost to the HTML
+    let entry = document.createElement('div');
+    entry.innerHTML = `<div class="mouser-bom-total" style="text-align: center; background-color: #dff0d8;"><span style="font-weight: bold; font-size: 16px; margin-top: 8px; margin-bottom: 8px;" class="mouser-bom-total value">TOTAL ${formatter.format(
+      totalPrice
+    )}</span></div>`;
+
+    // Insert a new row on top of the table
+    document.querySelector('.bom.hidden-for-nojs').appendChild(entry);
+  } else {
+    document.querySelector(
+      '.mouser-bom-total.value'
+    ).innerText = `TOTAL ${formatter.format(totalPrice)}`;
+  }
 };
 
-updateWebsite();
+// updateWebsite();
+
+window.mouserUpdateTotalTimeout = null;
+document
+  .querySelector('.bom-table') //.hidden-for-nojs')
+  .addEventListener('DOMSubtreeModified', () => {
+    if (window.mouserUpdateTotalTimeout !== null) {
+      clearTimeout(window.mouserUpdateTotalTimeout);
+    }
+    window.mouserUpdateTotalTimeout = setTimeout(updateWebsite, 300);
+  });
